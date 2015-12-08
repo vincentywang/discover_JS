f369c20c9d90305b1fc08739bd60b271f2ab6dc4
vincentywang <vincentywang@gmail.com>
2015-12-07 23:29:50 -0500

    clear up and some comments

--- jQuery-30-D/3.7-Obligatory-Slider/slider.js
+++ jQuery-30-D/3.7-Obligatory-Slider/slider.js
@@ -2,20 +2,23 @@
 
 	var sliderUl = $('div.slider').css('overflow', 'hidden').children('ul'),
 			imgs = sliderUl.find('img'),
-			imgWidth = imgs[0].width,
-			imgsLen = imgs.length,
+			imgWidth = imgs[0].width, // 500 
+			imgsLen = imgs.length, // 4
 			current = 1,
 			totalImgsWidth = imgWidth * imgsLen;
 
 	$('#slider-nav').show().find('button').on('click', function(){
-		console.log('click');
+		
 		var direction = $(this).data('dir'),
 				loc = imgWidth; //500
 
-		(direction==='next') ? ++current : --current;
+		if (direction === 'next') {
+			++ current;
+		} else {
+			-- current;
+		}
 
-		// reverse pass the first image
-		if (current === 0) {
+		if (current === 0) {		// reverse pass the first image
 			current = imgsLen;
 			direction = 'next';
 			loc = totalImgsWidth - imgWidth;
@@ -29,9 +32,9 @@
 	});
 
 	function transition (container, loc, direction) {
-		console.log('trans');
+
 		var unit;
-		if (direction && loc !==0) {
+		if (direction && loc !== 0) {
 			unit = (direction === 'next') ? '-=' : '+=';
 		}
 		container.animate({
