75f72e07d0dfa79341f4fe50b2b73f85a4c94258
vincentywang <vincentywang@gmail.com>
2016-03-29 22:32:40 +0800

    rewrite the event object

--- Modular-Javascript/pubsub_design_pattern/scripts/event.js
+++ Modular-Javascript/pubsub_design_pattern/scripts/event.js
@@ -1,5 +1,8 @@
 //events (publish subscribe) pattern
 
+/**
+
+
 var events = {
   events: {},
   on: function(eventName, fn) {
@@ -23,3 +26,43 @@ var events = {
     }
   }
 };
+
+
+*/
+
+
+
+var events = (function() {
+
+  var events = {};
+
+  function on(eventName, fn) {
+    this.events[eventName] = this.events[eventName] || [];
+    this.events[eventName].push(fn);
+  }
+
+  function off(eventName, fn) {
+    if (this.events[eventName]) {
+      for (var i = 0; i < this.events[eventName].length; i++) {
+        if (this.events[eventName][i] === fn) {
+          this.events[eventName].splice(i, 1);
+        }
+      }
+    }
+  }
+
+  function emit(eventName, data) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach(function(fn) {
+        fn(data);
+      });
+    }
+  }
+
+  return {
+    'on' : on,
+    'off' : off,
+    'emit' : emit
+  };
+
+})();
